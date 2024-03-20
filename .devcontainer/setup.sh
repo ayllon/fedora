5ad5af4f8747f4bb58b88d20d1525f72c990e73f
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2024-03-20 08:51:40 +0000

    Oh my zsh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,3 +2,12 @@
 
 echo "${SSH_PUBLIC_KEY}" > ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/authorized_keys
+
+git clone https://github.com/ayllon/dot.git ~/dot
+
+rm ~/.zsh*
+
+ln -s ~/dot/.gitconfig* ~
+ln -s ~/dot/.gitignore_global* ~
+ln -s ~/dot/.vimrc ~
+ln -s ~/dot/.zsh* ~
